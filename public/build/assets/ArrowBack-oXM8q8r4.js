590e5fdc5b01646e68b60218e5812c82fed9eb42
Nitin Goswami <sanyamgoswami21@gmail.com>
2024-08-01 17:08:44 +0530

    Production build

--- public/build/assets/ArrowBack-oXM8q8r4.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./Typography-DQvRHjr3.js";import{r as t}from"./createSvgIcon-BYmNDG3k.js";import{j as a}from"./app-D8U5PHYV.js";var r={},o=e;Object.defineProperty(r,"__esModule",{value:!0});var u=r.default=void 0,i=o(t()),f=a;u=r.default=(0,i.default)((0,f.jsx)("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"}),"ArrowBack");export{u as d};
--- public/build/assets/useTheme-DGMkIOkT.js
+++
@@ -1,1 +0,0 @@
-import"./app-D8U5PHYV.js";import{E as m,H as t}from"./Typography-DQvRHjr3.js";import{a as o}from"./Box-q44rX2z1.js";function u(){const e=o(t);return e[m]||e}export{u};
